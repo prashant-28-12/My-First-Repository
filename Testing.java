a44099d4e9f03c0985d170429b6839fef0f01e6f
Prashant <prashantgaya18@gmail.com>
2023-12-17 14:21:14 +0530

    print hello world

---
+++ Testing.java
@@ -0,0 +1,11 @@
+
+public class print{
+    public static showMessage(){
+        System.out.println("Hello World");
+    }
+}
+public class Testign{
+    public static void main(String agrs[]){
+      print.showMessage();  
+    }
+}
